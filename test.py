5a195c32472eacc142ceed4e0f003cdf9921405d
ubuntu4bus <ubuntu4bus@gmail.com>
2018-05-04 00:35:53 +0800

    {'Date': 'x-date: Fri, 04 May 2018 00:34:58 GMT', 'Authorization': 'hmac username="37ea2500589b4bfba83109e994fd4828",algorithm="hmac-sha1",headers="x-date",signature="ogx1TVhBD9mffAEDceEuA4Zs0TY="'} Response code: 403
    {"message":"HMAC signature cannot be verified, a valid date or x-date header is required for HMAC Authentication"}

--- test.py
+++ test.py
@@ -1,13 +1,39 @@
-from kong_hmac import generate_request_headers
 import requests
+import base64
+import hashlib
+import hmac
+from datetime import datetime
+
+def sha1_hash_base64(string_to_hash, secret):
+    #string_to_hash = "x-date: Thu, 03 May 2018 16:07:13 GMT"
+    h = hmac.new(secret, (string_to_hash).encode('utf-8'), hashlib.sha1)
+    return base64.b64encode(h.digest())
 
 
 url = "http://ptx.transportdata.tw/MOTC/v2/Bus/RealTimeByFrequency/City/Taipei?$format=JSON"
-key_id = "b30cc9bf581d48e6bf2cf9cc10d0aa19"
-secret = b"JXU_hVy_NpApRP2VPMwqSAqLV7s"
+key_id = "37ea2500589b4bfba83109e994fd4828"
+secret = b"wBj14ITh9YcoRY4oJlyiSwshfQQ"
 
 
-get_request_headers = generate_request_headers(key_id, secret, url)
-r = requests.get(url, headers=get_request_headers)
+
+msg = "x-date: " + datetime.now().strftime("%a, %d %b %Y %H:%M:%S GMT")
+
+sign_hmac = sha1_hash_base64(msg, secret)
+# Set the authorization header template
+auth_header_template = 'hmac username="{}",algorithm="{}",headers="{}",signature="{}"'
+# Set the signature hash algorithm
+algorithm = 'hmac-sha1'
+
+headers =  "x-date"
+auth_header = auth_header_template.format(key_id, algorithm, headers, sign_hmac.decode("utf-8"))
+request_headers = {
+            'Authorization': auth_header,
+            'Date': msg
+            }
+
+
+print(request_headers)
+r = requests.get(url, headers=request_headers)
+
 print ('Response code: %d\n' % r.status_code)
 print (r.text)
